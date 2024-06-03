b2eefe0be7779e4a27819bc5c0f3601eccf9fcf7
NGÔ THANH THIÊN <skycorn4@gmail.com>
2024-06-03 11:42:42 +0700

    update new

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,24 @@
+const http = require('http');
+const routes = require('./Routes/userRoute');
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const userRoutes = require('./Routes/userRoute')
 
 dotenv.config();
 
 const app = express();
-
+const server = http.createServer(app);
 
 app.use(express.json());
 app.use(cors());
-
-app.use('/api', userRoutes);
+app.use("/api/v1", routes);
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>console.log("Connected success"))
-.catch((err) => console.log(err))
-
-app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running in ${process.env.PORT}`);
+.then(()=>{
+    console.log("Connected");
+    server.listen(process.env.PORT, ()=>{
+        console.log(`Server is running in ${process.env.PORT}`);
+    })
 })
+.catch((err) => console.log(err))
